8c7b129a515891460cf9dbd976ac35e84c347424
Orlando Diaz <orlandodiazconde@gmail.com>
2023-05-03 18:15:17 -0500

    Add input validation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [values, setValue] = useState([
-    { id: 0, value: 0 },
-    { id: 1, value: 0 },
-    { id: 2, value: 0 },
+    { id: 0, value: "" },
+    { id: 1, value: "" },
+    { id: 2, value: "" },
   ]);
-  const ids = [0, 1, 2];
-  console.log(values);
+
   function setVote(id, newValue) {
-    console.log("called");
     setValue(
       values.map((value) => {
-        console.log(id, value.id);
-        if (value.id === id) return { id, value: Number(newValue) };
+        if (value.id === id) return { id, value: newValue };
         return value;
       })
     );
@@ -27,27 +24,45 @@ export default function Home() {
       className={`h-screen grid place-content-center bg-red-500 ${inter.className}`}
     >
       <section className="space-x-1">
-        {ids.map((id) => {
-          return <Input key={id} id={id} setVote={setVote} />;
+        {values.map(({ id, value }) => {
+          return <Input key={id} id={id} voteValue={value} setVote={setVote} />;
         })}
-        Total: {values.reduce((acc, curr) => acc + curr.value, 0)}
+        Total: {values.reduce((acc, curr) => acc + Number(curr.value), 0)}
       </section>
     </main>
   );
 }
 
-function Input({ setVote, id }) {
+function Input({ setVote, id, voteValue }) {
+  function handleKeyDown(ev) {
+    const code = ev.keyCode;
+    if (code === 8 || code === 46) {
+      setVote(id, "");
+      return;
+    }
+    if (code === 32 || code === 110) {
+      if (voteValue.slice(-2) === ".5") return;
+      setVote(id, `${voteValue ? voteValue : "0"}.5`);
+      return;
+    }
+  }
+  function handleChange(ev) {
+    const currentValue = ev.target.value;
+    const notValid =
+      currentValue.length > 3 ||
+      Number(currentValue) > 9 ||
+      Number(currentValue) < 0;
+    if (notValid) return;
+    setVote(id, currentValue);
+  }
   return (
     <input
       type="number"
-      onChange={(ev) => {
-        let value = ev.target.value;
-        if (ev.target.value > 9) {
-          value = 9;
-        }
-        setVote(id, value);
-      }}
-      className="w-8 p-1 rounded-sm"
+      value={voteValue}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      step="0.5"
+      className="w-16 p-1 rounded-sm"
     ></input>
   );
 }
